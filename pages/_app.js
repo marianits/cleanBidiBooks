0464fc9e718ac829425131fd79e9cfe9bce900bb
Mariana Arnez <mariana.arnez99@gmail.com>
2023-01-09 21:30:21 -0400

    Install semantic-ui

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,12 @@
 import '../styles/globals.css'
+import 'semantic-ui-css/semantic.min.css';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <main>
+      <Component {...pageProps} />
+    </main>
+  )
 }
 
 export default MyApp
