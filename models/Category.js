54dcc9174ed30816d8d49ed5c064cdfa32595148
Mariana Arnez <mariana.arnez99@gmail.com>
2023-01-09 21:29:52 -0400

    Add the category model

---
+++ models/Category.js
@@ -0,0 +1,17 @@
+import { Schema, model, models } from 'mongoose';
+
+const categorySchema = new Schema({
+  nombre: {
+    type: String,
+    required: true,
+    trim: true
+  },
+  descripcion: {
+    type: String,
+    trim: true
+  },
+  timestamps: true,
+  versionKey: false
+})
+
+export default models.Category || model('Category', categorySchema)
