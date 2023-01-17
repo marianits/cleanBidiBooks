40eb840a8f0de99df7fd44f6f8e8cd7a71dea171
Mariana Arnez <mariana.arnez99@gmail.com>
2023-01-17 19:43:15 -0400

    Change main layout

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,15 @@
 import '../styles/globals.css'
 import 'semantic-ui-css/semantic.min.css';
+import { QueryClient, QueryClientProvider } from 'react-query';
+
+const client = new QueryClient();
 
 function MyApp({ Component, pageProps }) {
   return (
     <main>
-      <Component {...pageProps} />
+      <QueryClientProvider client={client}>
+        <Component {...pageProps} />
+      </QueryClientProvider>
     </main>
   )
 }
