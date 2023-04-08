74d3cf8cf488fbf045cf04212e20973e82cc6d15
Mariana Arnez <mariana.arnez99@gmail.com>
2023-04-08 10:30:22 -0400

    Change props on the book page

--- pages/admin/books/[id].js
+++ pages/admin/books/[id].js
@@ -1,7 +1,7 @@
 import { useState } from 'react'
 import { ReactReader } from "react-reader"
 
-export default function Book({ book: { URL } }){
+export default function Book({ book: { fileURL } }){
   const [location, setLocation] = useState(null)
   const locationChanged = (epubcifi) => {
     // epubcifi is a internal string used by epubjs to point to a location in an epub. It looks like this: epubcfi(/6/6[titlepage]!/4/2/12[pgepubid00003]/3:0)
@@ -12,7 +12,7 @@ export default function Book({ book: { URL } }){
       <ReactReader
         location={location}
         locationChanged={locationChanged}
-        url={URL}
+        url={fileURL}
       />
     </div>
   )
