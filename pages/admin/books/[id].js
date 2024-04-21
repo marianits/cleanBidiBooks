d270da52b0ba84ad535a138c01b6a07c95dea3a4
Mariana Arnez <mariana.arnez99@gmail.com>
2024-04-21 19:16:11 -0400

    Add the highligh option to the text

--- pages/admin/books/[id].js
+++ pages/admin/books/[id].js
@@ -1,18 +1,53 @@
-import { Button, Icon } from 'semantic-ui-react';
 import { useEffect, useState, useRef } from 'react';
+import { Button, Icon } from 'semantic-ui-react';
 import { ReactReader } from "react-reader";
 
 export default function Book({ book: { fileURL } }){
   const [location, setLocation] = useState(null);
   const [largeText, setLargeText] = useState(false);
-  const rendition = useRef(undefined);
+  const [rendition, setRendition] = useState(undefined)
+  const [selections, setSelections] = useState([]);
+  const renderition = useRef(undefined);
+
+  // Function for the font settings:
   useEffect(() => {
-    rendition.current?.themes.fontSize(largeText ? '140%' : '100%')
+    renderition.current?.themes.fontSize(largeText ? '140%' : '100%')
   }, [largeText])
+
+  useEffect(() => {
+    if (rendition) {
+      function setRenderSelection(cfiRange, contents) {
+        if (rendition) {
+          setSelections((list) =>
+            list.concat({
+              text: rendition.getRange(cfiRange).toString(),
+              cfiRange,
+            })
+          )
+          rendition.annotations.add(
+            'highlight',
+            cfiRange,
+            {},
+            undefined,
+            'hl',
+            { fill: 'orange', 'fill-opacity': '0.5', 'mix-blend-mode': 'multiply' }
+          )
+          const selection = contents.window.getSelection()
+          selection?.removeAllRanges()
+        }
+      }
+      rendition.on('selected', setRenderSelection)
+      return () => {
+        rendition?.off('selected', setRenderSelection)
+      }
+    }
+  }, [setSelections, rendition])
+
   const locationChanged = (epubcifi) => {
     // epubcifi is a internal string used by epubjs to point to a location in an epub. It looks like this: epubcfi(/6/6[titlepage]!/4/2/12[pgepubid00003]/3:0)
     setLocation(epubcifi)
   }
+
   return (
     <div style={{ height: "100vh" }}>
       <Button icon basic color='orange' onClick={() => setLargeText(!largeText)}>
@@ -23,7 +58,7 @@ export default function Book({ book: { fileURL } }){
         locationChanged={locationChanged}
         url={fileURL}
         getRendition={(_rendition) => {
-          rendition.current = _rendition
+          renderition.current = _rendition
           _rendition.hooks.content.register((contents) => {
             const body = contents.window.document.querySelector('body')
             if (body) {
@@ -32,7 +67,8 @@ export default function Book({ book: { fileURL } }){
               }
             }
           })
-          rendition.current.themes.fontSize(largeText ? '140%' : '100%')
+          renderition.current.themes.fontSize(largeText ? '140%' : '100%')
+          setRendition(_rendition)
         }}
       />
     </div>
